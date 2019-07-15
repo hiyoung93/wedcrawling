426c29cdd83dbaa299f00672f7350e574938f07b
asd <asd>
2019-07-15 18:00:41 +0900

    전처리

--- 인스타 크롤 관련/단어 나누기.R
+++ 인스타 크롤 관련/단어 나누기.R
@@ -6,10 +6,12 @@ library(xlsx)
 library(RJavaTools)
 library(readxl) 
 
+
 getwd()
 setwd('C:/Users/709-000/Desktop/wedcrawling/인스타 크롤 관련')
 
-df <-read_excel("instagram.xlsx",sheet = "Sheet1");df
+ df <-read_excel("instagram.xlsx",sheet = "Sheet1");df# 이거는 잘나오는데 마지막 인코딩에서 ㅄ됨,,
+df <- read.table('tag.csv',fileEncoding='utf-32')
 
  # a <- str_split(df, "#") %>% .[[1]] # 열 나눠주기
 
@@ -20,13 +22,27 @@ hs <- unlist(hs);hs
 s <- strsplit(hs, "#")
 s <- data.frame(s)
 
-length(s$data21)
-
 str(s)
 s
 
 str_length(s) # 전체 글자수 알려주기
 nchar(s) # 각 행마다 글자수 알려주기
-tolower(s) # 소문자로 만들기
-str_trim(s)# 공백 없애기
+s<- tolower(s) # 소문자로 만들기
+s<- str_trim(s)# 공백 없애기
 View(s)
+
+s <- unlist(s)
+getOption("encoding")
+save(s,file = 'gello', Encoding='utf-32')
+options(encoding='utf-32')
+
+write.table(s,file ='df.txt',fileEncoding='utf-32',sep='')
+
+
+write.csv(s, file = 'my_data.txt')
+
+str(s)
+
+write(s,
+       file = "my_data.txt",
+       sep = ",")
