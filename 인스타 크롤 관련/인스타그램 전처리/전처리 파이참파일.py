69e52e5ec40f16a6dcfe7dff21fa7933a10bd8c3
hi <skarnd93@gmail.com>
2019-07-24 17:28:41 +0900

    asd

--- 인스타 크롤 관련/인스타그램 전처리/전처리 파이참파일.py
+++ 인스타 크롤 관련/인스타그램 전처리/전처리 파이참파일.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import re
+import string
 
 df = pd.read_csv('s.csv',encoding='utf-16') # 저장한 csv파일을 가지고 와서 인코딩값을 utf-16으로 주고 가져오기
 
 tag = df['data2'] # tag 안에 data2 col넣기
 tag = tag.tolist() # list 만들기
+
+result = tag.findall('tag.txt')
+
+word = tag[1].strip(string.punctuation+' “')
+tag = word.split('#')
+tag = re
 
 f = open('out.txt', 'w', -1, 'utf-8')
 
